165cbdef409b4d14e66e51fd4289600c02fdb793
Diego <contato.digpos@gmail.com>
2021-06-08 15:59:33 -0300

    Palavra reservada Base

--- ByteBank/Funcionarios/Funcionario.cs
+++ ByteBank/Funcionarios/Funcionario.cs
@@ -12,7 +12,7 @@ namespace ByteBank.Funcionarios
         public string CPF { get; set; }
         public double Salario { get; set; }
         
-        public virtual GetBonificacao()
+        public virtual double GetBonificacao()
         {
             return Salario * 0.10;
         }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -19,11 +19,17 @@ namespace ByteBank
             carlos.Salario = 2000;
 
             gerenciador.Registrar(carlos);
-
+            
             Diretor roberta = new Diretor();
             roberta.Nome = "Roberta";
             roberta.CPF = "454.658.148-3";
             roberta.Salario = 5000;
+
+            Funcionario robertaTeste = roberta;
+
+            Console.WriteLine("Bonificacao de uma referencia de Diretor: " + roberta.GetBonificacao());
+            Console.WriteLine("Bonificacao de uma referencia de Funcionario: " + robertaTeste.GetBonificacao());
+
 
             gerenciador.Registrar(roberta);
 
